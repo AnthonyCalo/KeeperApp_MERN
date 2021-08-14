0a0b767a25190dcb43e2192cf73f74e37cff231e
paulie-walnuts <acalo4554@gmail.com>
2021-08-14 13:07:28 -0400

    clean up

--- server/serverApp.js
+++ server/serverApp.js
@@ -11,7 +11,6 @@ app.use(express.urlencoded({
     extended: true
   }));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'build', 'index.html')));
 
